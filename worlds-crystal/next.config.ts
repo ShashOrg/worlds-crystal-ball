24915bff553368cab60ff8398bf285abfcdbb6fb
Shashakar <dextarm@gmail.com>
2025-10-20 22:20:48 -0600

    Restrict image remote patterns to GitHub and Google

--- worlds-crystal/next.config.ts
+++ worlds-crystal/next.config.ts
@@ -6,23 +6,15 @@ const nextConfig: NextConfig = {
       {
         protocol: "https",
         hostname: "avatars.githubusercontent.com",
-        pathname: "/u/**",
+        pathname: "/**",
       },
       {
         protocol: "https",
         hostname: "lh3.googleusercontent.com",
         pathname: "/**",
       },
-      {
-        protocol: "https",
-        hostname: "pbs.twimg.com",
-        pathname: "/**",
-      },
     ],
     formats: ["image/avif", "image/webp"],
-    dangerouslyAllowSVG: false,
-    contentDispositionType: "inline",
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
 };
 
